79df48a85f3ddddbcb7a2ebc2ab8c1617e758ec0
nayeem202 <nayeem.ahmedemba@gmail.com>
2022-12-27 17:03:05 +0600

    first commit

---
+++ src/main/java/com/example/reactiveprogrammingallpractice/ReactiveProgrammingAllPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactiveprogrammingallpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingAllPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveProgrammingAllPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactiveprogrammingallpractice/ReactiveProgrammingAllPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactiveprogrammingallpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingAllPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
